3bfd9994b97f67517ea4719aceaf806a23105e67
littleroyboy <roiboon.cc@gmail.com>
2021-06-11 03:33:44 +0700

    fix: db connection string

--- scripts/db/create.sh
+++ scripts/db/create.sh
@@ -1,4 +1,4 @@
 source .env
 
-echo "CREATE DATABASE ${DATABASE_USER}_development" | psql -U $DATABASE_USER
-echo "CREATE DATABASE ${DATABASE_USER}_test" | psql -U $DATABASE_USER
+echo "CREATE DATABASE ${DATABASE_NAME}_development;" | psql -U $DATABASE_USER
+echo "CREATE DATABASE ${DATABASE_NAME}_test;" | psql -U $DATABASE_USER
--- scripts/db/drop.sh
+++ scripts/db/drop.sh
@@ -1,4 +1,4 @@
 source .env
 
-echo "DROP DATABASE ${DATABASE_USER}_development" | psql -U $DATABASE_USER
-echo "DROP DATABASE ${DATABASE_USER}_test" | psql -U $DATABASE_USER
+echo "DROP DATABASE ${DATABASE_NAME}_development;" | psql -U $DATABASE_USER
+echo "DROP DATABASE ${DATABASE_NAME}_test;" | psql -U $DATABASE_USER
